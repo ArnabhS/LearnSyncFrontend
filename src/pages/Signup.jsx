469a0e6e0c961f03f477e36cefdec2d684311de1
Kane-dylan <cool.kdtech1234@gmail.com>
2024-10-18 01:29:35 +0530

    landing page backlight fix, login & signup padding fixed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -8,7 +8,7 @@ export default function Signup() {
     <div className="relative flex flex-col justify-center items-center min-h-screen">
       <div className="relative ">
         <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-64 h-64 bg-[#3EB1E1] rounded-full blur-3xl opacity-55 z-0"></div>
-        <div className="relative z-10 p-10">
+        <div className="relative z-10">
           <div className="bg-gradient-to-t from-[#09212B99] to-[#1D6F9199] text-[#DCECF2] flex flex-col justify-center items-center md:max-w-2xl h-[450px] gap-5 rounded-xl px-14 m-4">
             <h2 className="text-center text-4xl mb-8 font-semibold tracking-wide">
               Get Started
@@ -33,7 +33,7 @@ export default function Signup() {
               Signup
             </button>
             <p className=" tracking-wide text-center">
-              <span className="italic text-[#D9D9D9] hover:text-[#878787]">
+              <span className="italic text-xs text-[#D9D9D9] hover:text-[#878787]">
                 Already have an account ?
               </span>
               <Link to={"/login"} className="text-[#3EB1E1] cursor-pointer">
